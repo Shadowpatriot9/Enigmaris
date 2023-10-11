bd97f47f5109b83ab7f40bda25f0712cd4de4842
Shadowpatriot9 <gscovil9@gmail.com>
2023-10-11 12:49:22 -0600

    - Fixed Email Sharing and gtg for gmail accts only - Tested and declined multi-file for function modulation - Fixed minor formatting and conditional issues

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# ==================================
+# 			Introduction
+# ==================================
+
 """
 Allllrrrriiiggghhhtttt, this is pwd generator thingy. Take what you want, but just not my pwd (gl btw if you're
 trying to steal it lol)
@@ -7,14 +11,21 @@ trying to steal it lol)
 #       Dependencies
 # ================================
 
-import pyperclip as pc
 import string
 import secrets
+import time
+import os
+import smtplib
+import pyperclip as pc
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 # ================================
 #       Global Variables
 # ================================
 
+global pwd_final
+date = time.asctime()  # The date variable for options below
 
 # ================================
 #       Functions
@@ -33,7 +44,30 @@ def pwd_criteria_display():  # Communicates to users what current criteria/allow
 
     print(f"{crit}")  # Criteria display expression
 
+def pwd_generation():  # Generates password for users when executed
+    while True:
+        chr_vault_3 = "".join(map(str, chr_vault_2))  # List conversion to continuous string for secrets lib module gen
+
+        chr_count = int(input("\nEnter the Number of Characters for the New Password: "))  # Chr Amount Selection
+        pwd = "".join(secrets.choice(chr_vault_3) for i in range(chr_count))  # Pwd Generator
+
+        global pwd_final
+        pwd_final = pwd
+
+        if chr_count <= 100:
+            print(f"Password Generated: {pwd}")
+            pc.copy(pwd)  # Copy to Clipboard
+            print("\n***!Password Copied to Your Clipboard!***")
+            print("\n===================================")
+            break
+        elif chr_count > 100:  # Character limit determinant
+            print("***!Character amount too large. Enter a value less than or equal to 100!***")
+            continue
+
 def chr_vault_adjustment():  # Allows users to customize allowable characters for generation
+
+    # ***Need to make a multiple choice selection (more than one) for alteration if needed from user***
+
     print("\nWhat Would You Like to Alter?")
     choice = input("\nEnter 'L' to NOT include Letters in Password"
                    "\nEnter 'N' to NOT include Numbers in Password"
@@ -52,112 +86,62 @@ def chr_vault_adjustment():  # Allows users to customize allowable characters fo
         print("Special Characters Removed from the Next Generated Password.")
     print("===================================")
 
-def pwd_generation():  # Generates password for users when executed
-    while True:
-        chr_vault_3 = "".join(map(str, chr_vault_2))  # List conversion to continuous string for secrets lib module gen
-
-        chr_count = int(input("\nEnter the Number of Characters for the New Password: "))  # Chr Amount Selection
-        pwd = "".join(secrets.choice(chr_vault_3) for i in range(chr_count))  # Pwd Generator
-
-        if chr_count <= 100:
-            print(f"Password Generated: {pwd}")
-            pc.copy(pwd)  # Copy to Clipboard
-            print("\n***Password Copied to Your Clipboard***")
-            print("\n===================================")
-            break
-        elif chr_count >= 100:  # Character limit determinant
-            print("***!Character amount too large. Enter a value less than or equal to 100!***")
-            continue
-
 def final_options():  # Options for users after the main program has executed/concluded.
     final_choice = input("\nEnter 'S' to Share"
                          "\nEnter 'E' to Exit"
                          "\nEnter 'R' to Restart"
                          "\n\nPlease Enter an Option: ").lower()
+    print("\n===================================")
+
     if final_choice == "s":
-        """
-        # Options for sharing: email draft, URL or QR code, SMS, export to txt file (or PDF?)
-            print("Shared")
-            print("Choose a sharing option:")
-            print("1. Email")
-            print("2. URL or QR code")
-            print("3. SMS")
-            print("4. Export to text file")
-        
-            # Get user's choice
-            sharing_option = input("Enter the number of your choice: ")
-        
-            if sharing_option == "1":
-                # Code for sharing via email
-                print("Share via email")
-                
-                
-                import smtplib
-                from email.mime.text import MIMEText
-                from email.mime.multipart import MIMEMultipart
-                
-                def share_via_email(password, recipient_email):
+        print("\nEnter 'E' for Email"
+              "\nEnter 'T' for Export to Text file")
+
+        sharing_option = input("\nPlease Enter an Option: ").lower()  # Sharing Option Selection
+        print("\n===================================")
+
+        if sharing_option == "e":  # Email Sharing Option
+            def share_via_email(pwd, recipient_email):
+                while True:
                     # Email configuration
-                    sender_email = "your_email@gmail.com"
-                    sender_password = "your_password"
-                    subject = "Shared Password"
-                
+                    sender_email = input("Enter Sender's Email Address: ")
+                    sender_password = input("Enter Sender's Email Password: ")
+                    subject = f"Password Generated on {date}"
+
                     # Create the email content
                     message = MIMEMultipart()
                     message["From"] = sender_email
                     message["To"] = recipient_email
                     message["Subject"] = subject
-                    message.attach(MIMEText(password, "plain"))
-                
+                    message.attach(MIMEText(pwd, "plain"))
+
                     # Connect to the email server and send the message
                     with smtplib.SMTP("smtp.gmail.com", 587) as server:
                         server.starttls()
                         server.login(sender_email, sender_password)
                         server.sendmail(sender_email, recipient_email, message.as_string())
-                
-                # Usage:
-                recipient_email = "recipient@example.com"
-                share_via_email("YourGeneratedPassword123", recipient_email)
-                
-                        
-            elif sharing_option == "2":
-                # Code for sharing via URL or QR code
-                print("Share via URL or QR code")
-                     
-                        
-            elif sharing_option == "3":
-                # Code for sharing via SMS
-                print("Share via SMS")
 
-                from twilio.rest import Client
-                def share_via_sms(password, recipient_phone):
-                    account_sid = "your_account_sid"
-                    auth_token = "your_auth_token"
-                    client = Client(account_sid, auth_token)
-                    message = client.messages.create(
-                        body=f"Your shared password: {password}",
-                        from_="your_twilio_number",
-                        to=recipient_phone
-                    )
-                # Usage:
-                recipient_phone = "+1234567890"
-                share_via_sms("YourGeneratedPassword123", recipient_phone)
+                        print("\n===================================")
+                        print(f"\nPassword Successfully Sent to '{recipient_email}'")
+                        break
 
+            # Usage:
+            recipient_email = input("\nEnter Recipient Email Address: ")
+            share_via_email(pwd_final, recipient_email)
 
-            elif sharing_option == "4":
-                # Code for exporting to a text file
-                print("Export to text file")
-                
-                def export_to_text_file(password, filename):
-                with open(filename, "w") as file:
-                    file.write(password)
-                # Usage:
-                export_to_text_file("YourGeneratedPassword123", "password.txt")
+        elif sharing_option == "t":  # TXT File Export Option
+            filename = f"## Generated Password ## {date}.txt"
 
+            def export_text_file(pwd_final, filename):
+                desktop_path = os.path.expanduser("~/Desktop")
+                export_path = os.path.join(desktop_path, filename)
 
-            else:
-                print("Invalid choice. Please select a valid sharing option.")
-        """
+                with open(export_path, "w") as file:
+                    file.write(pwd_final)
+            # Usage:
+            export_text_file(pwd_final, filename)
+            print("\nText File Saved to Desktop")
+
     if final_choice == "e":
         quit()
     if final_choice == "r":
@@ -169,17 +153,17 @@ def final_options():  # Options for users after the main program has executed/co
 
 print("\n===================================")  # Intro Tagline
 print("\n'Welcome to Enigmaris'")
+print("\n===================================")
 
 while True:
     chr_vault_1 = [string.ascii_letters, string.digits, string.punctuation]  # Initially allowed characters(chr) for gen
     chr_vault_2 = chr_vault_1  # Used for chr_vault_adjustment() function to create an altered collection of chr (i.a.)
 
-    print("\n===================================")
-    print("\nEnter 'C' for Password Criteria Customization"  # Initial Selection Menu
-          "\nEnter 'P' to Generate Password")
-
     pwd_criteria_display()  # Pwd Criteria Display Function
 
+    print("\nEnter 'C' to Customize the Password Criteria"  # Initial Selection Menu
+          "\nEnter 'P' to Generate Password"
+          "\nEnter 'E' to Exit")
     initial_choice = input("\nPlease Enter an Option: ").lower()
     print("\n===================================")
 
@@ -188,6 +172,8 @@ while True:
         pwd_generation()
     elif initial_choice == 'p':  # Pwd Generation Function
         pwd_generation()
+    elif initial_choice == 'e':
+        exit()
 
     final_options()  # Final Options Function
 
